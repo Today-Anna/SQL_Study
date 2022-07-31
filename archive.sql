6dd567cb909d12ae59a585e51328f52b6c95a3b7
Today-Anna <110329712+Today-Anna@users.noreply.github.com>
2022-07-31 18:01:08 +0900

    Update archive.sql

--- archive.sql
+++ archive.sql
@@ -3,3 +3,9 @@ FROM [Categories]
 WHERE CategoryName != "Seafood"
 ORDER BY CategoryID DESC
 LIMIT 5
+
+SELECT FirstName
+FROM [Employees]
+WHERE LastName != "Peacock"
+AND EmployeeID < 7
+ORDER BY BirthDate ASC
